9041699cb4f8696333112496b73a952e96e5e0d3
蒋昊 <49517454+agreasjh@users.noreply.github.com>
2024-07-23 13:18:07 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -11,22 +11,11 @@ import { LocaleProvider } from '@/lib/locale'
 import { prepareDayjs } from '@/lib/dayjs'
 import { ThemeProvider } from '@/lib/theme'
 import Scripts from '@/components/Scripts'
-import { useEffect } from 'react'
 
 const Ackee = dynamic(() => import('@/components/Ackee'), { ssr: false })
 const Gtag = dynamic(() => import('@/components/Gtag'), { ssr: false })
 
 export default function MyApp ({ Component, pageProps, config, locale }) {
-  useEffect(() => {
-    // 明暗模式切换按钮的JavaScript代码
-    const toggleButton = document.querySelector('.toggle');
-    toggleButton.addEventListener('click', () => {
-      const isPressed = JSON.parse(toggleButton.getAttribute('aria-pressed'));
-      toggleButton.setAttribute('aria-pressed', !isPressed);
-      document.body.classList.toggle('dark');
-    });
-  }, []);
-
   return (
     <ConfigProvider value={config}>
       <Scripts />
@@ -40,23 +29,6 @@ export default function MyApp ({ Component, pageProps, config, locale }) {
               />
             )}
             {process.env.VERCEL_ENV === 'production' && config?.analytics?.provider === 'ga' && <Gtag />}
-            
-            {/* 明暗模式切换按钮开始 */}
-            <button className="toggle" aria-label="Toggle dark mode." aria-pressed="false">
-              <div className="eye eye-left">
-                <div className="eye__inner"></div>
-              </div>
-              <div className="eye eye-right">
-                <div className="eye__inner"></div>
-              </div>
-              <div className="nose"></div>
-              <div className="mouth">
-                <div className="lip lip__upper"></div>
-                <div className="lip lip__bottom"></div>
-              </div>
-            </button>
-            {/* 明暗模式切换按钮结束 */}
-            
             <Component {...pageProps} />
           </>
         </ThemeProvider>
